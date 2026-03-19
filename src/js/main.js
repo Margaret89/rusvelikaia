8c6d293984823d30ec2239bace073ee9e9a45773
match14 <match14@yandex.ru>
2026-03-20 01:11:26 +0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 
+//Swiper
+import Swiper from 'swiper';
+import { Navigation, Pagination, Thumbs } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 //Fancybox
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
@@ -65,4 +72,65 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 		});
 	});
+});
+
+// Верхний слайдер
+if(document.querySelector('.js-main-slider')){
+	const topSlider = new Swiper('.js-main-slider',
+	{
+		modules: [Navigation],
+		loop:true,
+		autoplay: 5000,
+		navigation: {
+			nextEl: '.js-main-slider-next',
+			prevEl: '.js-main-slider-prev',
+		},
+	});
+}
+
+//Переключение табов в меню каталога
+if(document.querySelector('.js-catalog-menu-sect-item')){
+	// Находим все элементы меню (вкладки)
+	const menuItems = document.querySelectorAll('.js-catalog-menu-sect-item');
+	// Находим все секции попапа
+	const popupSections = document.querySelectorAll('.js-catalog-menu-popup-sect');
+
+	// Вешаем обработчик клика на каждый пункт меню
+	menuItems.forEach(item => {
+		item.addEventListener('click', function() {
+			// Удаляем класс 'active' у всех пунктов меню
+			menuItems.forEach(menuItem => {
+				menuItem.classList.remove('active');
+			});
+
+			// Добавляем класс 'active' текущему пункту меню
+			this.classList.add('active');
+
+			// Получаем значение data-id текущего пункта меню
+			const popupTargetId = this.dataset.id; // получится "catalog-menu-sect-0"
+
+			// Удаляем класс 'active' у всех секций попапа
+			popupSections.forEach(section => {
+				section.classList.remove('active');
+			});
+
+			// Находим нужную секцию попапа и добавляем ей класс 'active'
+			const targetSection = document.querySelector(`.js-catalog-menu-popup-sect[data-id="${popupTargetId}"]`);
+			if (targetSection) {
+				targetSection.classList.add('active');
+			}
+		});
+	});
+}
+
+//Открыть мобильный сайдбар
+document.querySelector('.js-open-mobile-sidebar').addEventListener('click', function(e){
+	document.querySelector('.js-mobile-sidebar').classList.add('open');
+	document.querySelector('.js-body').classList.add('no-scroll');
+});
+
+//Закрыть мобильный сайдбар
+document.querySelector('.js-close-mobile-sidebar').addEventListener('click', function(e){
+	document.querySelector('.js-mobile-sidebar').classList.remove('open');
+	document.querySelector('.js-body').classList.remove('no-scroll');
 });
